a8c0f37d634e2b38f3dccfc7a534ccc74dd5e748
Martin Ndegwa <51788472+Martin-droid@users.noreply.github.com>
2021-04-10 22:43:09 +0300

    Add files via upload

---
+++ tablecontroller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\details;
+use App\Http\Controllers\search;
+use PDF;
+use App\Models\detail;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Models\tabledetails;
+
+
+class tablecontroller extends Controller
+{
+    //
+    function show()
+    {
+
+
+        $details= detail::all();
+
+        return view('tabledetails',['details'=>$details]);
+
+    }
+
+
+
+    public function downloadPDF()
+    {
+        $details= detail::all();
+        $pdf = PDF::loadview('tabledetails',compact('details'));
+        return $pdf->download('customers.pdf');
+    }
+
+}
